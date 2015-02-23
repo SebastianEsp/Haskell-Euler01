b1e62a101574306806730c6bc1cf69091fc6b8bc
Sebastian Esp <private@sebastianesp.dk>
2015-02-23 13:43:02 +0100

    Comments added

--- Euler01.hs
+++ Euler01.hs
@@ -1,1 +1,4 @@
 multiples = sum[x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
+-- x is assigned a list with numbers from 1 to 999
+-- Every number in this list is checked with division to see whether or not it is divisible by 3 or 5 
+-- The final sum of all the values in x are summed up
